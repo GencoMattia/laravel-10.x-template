aa208256de9f5ad5c95eecd39bfdb22a30669622
GencoMattia <gencomattia@hotmail.it>
2024-07-15 18:21:03 +0200

    fixed header's navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,18 +1,22 @@
 <header>
-    <div class="container">
-        <div class="row">
-            <section class="logo">
-                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="header-logo">
-            </section>
-            <nav class="navbar">
-                <ul>
-                    @foreach ( $navbarLinks as $link )
-                    <li>
-                        {{ $link["title"] }}
-                    </li>
-                    @endforeach
-                </ul>
-            </nav>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid justify-content-center">
+            <div class="navbar">
+                <a class="navbar-brand me-5" href="#">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="header-logo">
+                </a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        @foreach ( $navbarLinks as $link )
+                        <li class="nav-item me-3">
+                            <a class="nav-link active" aria-current="page" href="#">
+                                {{ $link["title"] }}
+                            </a>
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
+    </nav>
 </header>

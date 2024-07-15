960c0f60df49d9fa9d019693830a34722650b7d1
GencoMattia <gencomattia@hotmail.it>
2024-07-15 16:09:07 +0200

    added img files and working on header partial

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <section class="logo">
+                <img src="../assets/img/dc-logo.png" alt="header-logo">
+            </section>
+            <nav class="navbar">
+                <ul>
+                    <li>
+
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
